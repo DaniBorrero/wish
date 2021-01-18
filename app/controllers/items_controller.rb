6a2a5dfbb161b964ff6d3fa22b8e9eb9619a3481
daniel <borrerodaniel89@gmail.com>
2021-01-18 19:46:16 -0300

    finish views and controller - model list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,64 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, onyl: [:edit, :update, :new, :create, :show]
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new    
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
-  def create
+  def create 
+    @item = Item.new(item_params)
+    
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+        
+      end
+    end
   end
 
   def update
+    @item = Item.find(params[:id])
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :url, :list_id)  
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,64 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!, onyl: [:edit, :update, :new, :create, :show]
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new    
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
-  def create
+  def create 
+    @list = List.new(list_params)
+    @list.event_id = params[:event_id]
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to @list, notice: 'list was successfully created.' }
+        format.json { render :show, status: :created, location: @list }
+      else
+        format.html { render :new }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+        
+      end
+    end
   end
 
   def update
+    @list = List.find(params[:id])
+    respond_to do |format|
+      if @list.update(list_params)
+        format.html { redirect_to @list, notice: 'list was successfully updated.' }
+        format.json { render :show, status: :ok, location: @list }
+      else
+        format.html { render :edit }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    respond_to do |format|
+      format.html { redirect_to lists_url, notice: 'list was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :description, :event_id)  
   end
 end
